413352ea4a857d68e3c536b62ceef79259efbe8f
victorli112 <victor.li.7802@gmail.com>
2024-03-22 14:12:45 -0400

    a5

---
+++ App.js
@@ -0,0 +1,11 @@
+import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+Hello(app);
+Lab5(app);
+app.listen(4000);
